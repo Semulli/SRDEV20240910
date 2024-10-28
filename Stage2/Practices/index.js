1e80bca93b836ce8b4fbaea78997d463ce53438b
abdulazizsama <semabdulazizova@gmail.com>
2024-10-28 18:25:07 +0400

    updated

---
+++ Stage2/Practices/index.js
@@ -0,0 +1,67 @@
+// var a = 10;
+
+// console.log(a);
+
+//?operations
+// var a = "4";
+// var b = 5;
+
+// var result = a + b;
+// console.log(result);
+
+// var result = a - b;
+// console.log(result);
+
+//? var result = a * b;
+//? console.log(result);
+
+// ! var result = a / b;
+// ! console.log(result);
+
+//const age = 24;
+// const age = 10;
+
+// if (age > 18) {
+//   console.log("You can create an account!");
+// } else {
+//   console.log("Please, don't try again!!!");
+// }
+
+//? var age = 24;
+//? var isExperience = false;
+
+//? if (age > 18 || isExperience == true) {
+//?   console.log(" You hire a new job now");
+//? } else {
+//?   console.log("Get out!!!!!");
+// }
+
+// var age = 24;
+// var isExperience = false;
+
+// if (age > 18 && isExperience == true) {
+//   console.log(" You are hired a new job now");
+// } else {
+//   console.log("Get out!!!!!");
+// }
+
+//alert("be careful");
+
+//prompt("how are you doing today??");
+
+// confirm("block notifications?");
+
+// var a = 10;
+// a **= 7;
+// console.log(a);
+
+const a = 409;
+
+var result = a % 10;
+
+console.log(result);
+
+var customerName = `hi 
+mine name is sama
+`;
+console.log(customerName);
