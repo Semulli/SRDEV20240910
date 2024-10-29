b663a37b29b04c2868dccb8fe91729e698091e91
abdulazizsama <semabdulazizova@gmail.com>
2024-10-29 08:33:43 +0400

    Create new js file

---
+++ Stage2/CLass exercises/part1/index.js
@@ -0,0 +1,78 @@
+// let child = 4;
+// let Name = "Dakota";
+// let Location = "England";
+// let Job = "Software Developer";
+
+//  task1 console.log(
+//   "you will be a " +
+//     Job +
+//     " in " +
+//     Location +
+//     " and married to " +
+//     Name +
+//     " with " +
+//     child +
+//     " child. "
+// );
+
+// Store your birth year in a variable.
+// Store a future year in a variable.
+// Calculate your 2 possible ages for that year based on the stored values.
+// For example, if you were born in 1988, then in 2026 you'll be either 37 or 38, depending on what month it is in 2026.
+// Output them to the screen like so: "I will be either NN or NN in YYYY", substituting the values.
+
+//  task2 let BirthOfDate = 2004;
+
+// let Future = 2040;
+
+// let age1 = Future - BirthOfDate;
+
+// let age2 = age1 - 1;
+
+// console.log("You will be either " + age1 + " or " + age2 + " in " + Future);
+
+// Store your current age into a variable.
+// Store a maximum age into a variable.
+// Store an estimated amount per day (as a number).
+// Calculate how many you would eat total for the rest of your life.
+// Output the result to the screen like so: "You will need NN to last you until the ripe old age of X".
+
+//  task3 let MyAge = 20;
+
+// let MaxAge = 80;
+
+// let daily = 2;
+
+// let num = MaxAge - MyAge;
+
+// let total = num * daily * 365;
+
+// console.log(total);
+
+//task4
+
+// Store a radius into a variable.
+// Calculate the circumference based on the radius, and output "The circumference is NN".
+// Calculate the area based on the radius, and output "The area is NN".
+
+// let radius = 10;
+// let Math.PI = 3.14;
+// let circumference = 2 * radius * pi;
+// console.log(" The circumference is " + circumference);
+// let area = pi * radius * radius;
+// console.log(" the area is " + area);
+
+// Store a celsius temperature into a variable.
+// Convert it to fahrenheit and output "NN°C is NN°F".
+// Now store a fahrenheit temperature into a variable.
+// Convert it to celsius and output "NN°F is NN°C."
+
+// task5;
+let C = 20;
+let F = (9 / 5) * C + 32;
+
+console.log(C + " is " + F);
+
+let f = 10;
+let c = (5 / 9) * (f - 32);
+console.log(f + " is " + c);
